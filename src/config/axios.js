b1550b77c8cad028527c7ca55dee93ddd5ab7aa5
Jorge Acosta <jorge.acosta@culqi.com>
2024-02-19 01:57:54 -0500

    cambio de hosting

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080/api/v1', 
+    baseURL_local: 'http://localhost:8080/api/v1', 
+    baseURL: 'https://stampiza2-server.onrender.com/api/v1', 
 });
 
 export default axiosInstance;
